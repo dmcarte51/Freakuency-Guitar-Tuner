02fe2c9adfd703048afa41460d10eae11499fd53
dmcarte51 <dcarte51@msudenver.edu>
2023-06-23 17:54:34 -0600

    add tuner fragment and edit main activity

--- app/src/main/java/com/example/freakuencyguitartuner/MainActivity.kt
+++ app/src/main/java/com/example/freakuencyguitartuner/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.freakuencyguitartuner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+
+            findViewById<FragmentContainerView>(R.id.fragment_container)?.let { frameLayout ->
+                val questionsFragment = TunerFragment()
+                supportFragmentManager.beginTransaction()
+                    .add(frameLayout.id, questionsFragment).commit()
+            }
+        }
     }
 }
